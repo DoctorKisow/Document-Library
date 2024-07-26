64b5358b6f2e7fba3aea0be6bb1cb8b71bdceafa
Dr. Matthew Kisow <33046135+DoctorKisow@users.noreply.github.com>
2024-07-26 09:00:34 -0400

    Create markdown-docs.js
    Attempt at a TOC.

---
+++ Document Library/markdown-docs.js
@@ -0,0 +1,19 @@
+module.exports = {
+  page: {
+    layout: 'default',
+    toc: true
+  },
+  site: {
+    editSourceUrl: 'https://github.com/DoctorKisow/Document-Library/tree/main',
+    title: 'Document Library',
+    url: 'https://github.com/DoctorKisow/Document-Library',
+  },
+  template: {           // set template specific information
+    path: 'default',
+    cssVars: {
+      brandColor: '#00A288',
+      brandColorLight: '#8BDBCD',
+      brandColorDark: '#00382F'
+    }
+  }
+}
